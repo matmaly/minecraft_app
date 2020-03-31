810e2e532bf2e8ebe900f41e9852193150c2236b
matmaly <mateuszfmaly@gmail.com>
2020-03-31 14:11:10 +0000

    Implementing start,stop functions in php

--- index.php
+++ index.php
@@ -1,41 +1,30 @@
 <html>
 	<head>
 		<title>Chuje Z ESO - Command Centre</title>
-		<script type="text/javascript">
-
-			function sh(input) {
-				var exec = require('child_process').exec, child;
-				child = exec('echo ' + input,
-				    function (error, stdout, stderr) {
-				    	document.getElementById("testId").innerText = stdout
-				        // console.log('stdout: ' + stdout);
-				        // console.log('stderr: ' + stderr);
-				        // if (error !== null) {
-				        //      console.log('exec error: ' + error);
-				        // }
-				    });
-				 child();
-			}
-
-			function startServer() {
-				sh("Start")
-			}
-
-			function stopServer() {
-				sh("Stop")
-			}
-			
 		</script>
 	</head>
 
 	<body>
 		<img src="logo.jpg" alt="Lights" style="width:25%">
-		<button id="startBtn" type="button" onclick="startServer()">Start</button>
-		<button id="stopBtn" type="button" onclick="stopServer()">Stop</button>
+		<form method="post">
+		<input type="submit" name="start" id="start" value="Start" /><br/>
+		<input type="submit" name="stop" id="stop" value="Stop" /><br/>
+		</form>
 		<p id="testId"></p>
 		<?php
+			function startServer()	{
+				echo "Start";
+			}
+			function stopServer()   {
+				echo "Stop";
+			}
+			if(array_key_exists("start", $_POST))   {
+				startServer();
+			}
+			if(array_key_exists("stop", $_POST))   {
+				stopServer();
+			}
 			$output = shell_exec('/var/www/html/oci-cl_scripts/test');
-		  	echo "<pre>$output</pre>";
 		?>
 	</body>
 </html>
